dca1227f0a314df879d59451af4cbd418c96a343
heybereket <semagnbereket@gmail.com>
2023-04-04 23:02:37 -0400

    animation

--- components/season/SeasonCard.tsx
+++ components/season/SeasonCard.tsx
@@ -3,7 +3,7 @@ export const SeasonCard = (props: any) => {
       <div
         className={`${props.className} ${
           props.className === "chargedUp" && "hover:bg-gray-400"
-        } hover:cursor-pointer border-solid border-2 border-primary rounded-lg md:w-[800px] w-[350px] px-10 py-24`}
+        } hover:cursor-pointer hover:bg-gray-400 border-solid border-2 border-primary rounded-lg md:w-[800px] w-[350px] px-10 py-24`}
       >
         <h1 className="text-4xl font-bold text-center mb-2">{props.title}</h1>
         <p className="text-center text-gray-300 font-bold">
