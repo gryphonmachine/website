7c5bc11b10ed0e62cc4f13685359a9003ad32fa7
heybereket <semagnbereket@gmail.com>
2023-04-05 23:45:51 -0400

    code cleanup

--- pages/events.tsx
+++ pages/events.tsx
@@ -1,6 +1,7 @@
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 import { MediaImage } from "@/components/MediaImage";
+import { Events } from "@/lib/Events";
 import Head from "next/head";
 
 export default function EventsPage() {
@@ -16,69 +17,17 @@ export default function EventsPage() {
       />
 
       <div className="flex flex-col justify-center md:grid md:grid-cols-3 gap-5 pr-10 pl-10">
-        <MediaImage
-          title="Discovery Day Workshop with Glenforest STEM"
-          subtitle="December 2022"
-          img="/events/dec2022.jpg"
-        />
-        <MediaImage
-          title="Nahani Way LEGO Robotics Mentorship Program with Glenforest STEM"
-          subtitle="2017-2018 School Year"
-          img="/events/Image-16.jpg"
-        />
-        <MediaImage
-          title="FIRST LEGO League Team Mentorship - Team 20475"
-          img="/events/Lego-768x432.jpg"
-        />
-        <MediaImage
-          title="Polycultural Mentorship with Teams 1241, 771, 4939, and 4001"
-          img="/events/Polycultural-Workshop-Week-1-PowerPoint.ppt.jpg"
-        />
-        <MediaImage
-          title="Polycultural Day Camp"
-          subtitle="August 2017"
-          img="/events/Image-5-8.jpg"
-        />
-        <MediaImage
-          title="Paint the Town Red Parade"
-          subtitle="July 2017"
-          img="/events/Image-6-6.jpg"
-        />
-        <MediaImage
-          title="Canadian Youth STEM Conference"
-          subtitle="May 2017"
-          img="/events/Image-5-6.jpg"
-        />
-        <MediaImage
-          title="Nahani Way LEGO Robotics Engineering Mentorship Grand Finale"
-          subtitle="May 2017"
-          img="/events/Image-5-5.jpg"
-        />
-        <MediaImage
-          title="Minister Bains Innovation Week Visit"
-          subtitle="May 2017"
-          img="/events/Image-11-1.jpg"
-        />
-        <MediaImage
-          title="Do you want to CAD a Snowman?"
-          subtitle="December 2016"
-          img="/events/Image-1-6.jpg"
-        />
-        <MediaImage
-          title="Hour of Code Day"
-          subtitle="December 2016"
-          img="/events/Image-2-8.jpg"
-        />
-        <MediaImage
-          title="Nahani Way P.S LEGO Robotics Engineering Leadership Mentorship"
-          subtitle="November 2016 - May 2017"
-          img="/events/Image-7.jpg"
-        />
-        <MediaImage
-          title="SWATPOSIUM"
-          subtitle="November 2016"
-          img="/events/Image-1-3.jpg"
-        />
+        {Events.map((event: any) => {
+          return (
+            <>
+              <MediaImage
+                title={event.title}
+                subtitle={event.subtitle}
+                img={event.img}
+              />
+            </>
+          );
+        })}
       </div>
 
       <Footer />
--- pages/media.tsx
+++ pages/media.tsx
@@ -5,6 +5,7 @@ import { Subtitle } from "@/components/Subtitle";
 import { Title } from "@/components/Title";
 import { VideoEmbed } from "@/components/VideoEmbed";
 import Head from "next/head";
+import { MediaList } from "@/lib/Media";
 
 const ViewAlbum = (props: any) => {
   return (
@@ -52,75 +53,22 @@ export default function MediaPage() {
       <Title className="pl-10">Photo/Album Highlights</Title>
 
       <div className="flex flex-col justify-center md:grid md:grid-cols-3 gap-5 pr-10 pl-10">
-        <div className="flex flex-col items-center ">
-          <Photo
-            className="opacity-50 md:h-[200px] md:w-[500px]"
-            img="/awards/28515219_1840517919346586_4816363233668407574_o.jpg"
-          />
+        {MediaList.map((media: any) => {
+          return (
+            <>
+              <div className="flex flex-col items-center ">
+                <Photo
+                  className="opacity-50 md:h-[200px] md:w-[500px]"
+                  img={media.img}
+                />
 
-          <ViewAlbum link="https://photos.google.com/share/AF1QipNc55Eumz_bkhC5WZdGbG2cj0ANmVu_i6czDTechAdE6_o-3rbPU5lHNiFxlRaTgw?key=X3BJZDlpQ0tGZTJWZGZvZ3Y2eFdjeFBHRl9Rb2J3" />
+                <ViewAlbum link={media.url} />
 
-          <Description
-            title="Durham College District Event"
-            subtitle="FIRST Power Up - 2018"
-          />
-        </div>
-
-        <div className="flex flex-col items-center ">
-          <Photo
-            className="opacity-50 md:h-[200px] md:w-[500px]"
-            img="/media/IMG_9920.jpg"
-          />
-
-          <ViewAlbum link="https://photos.app.goo.gl/ZY8h5piDbXfpuqKj1" />
-
-          <Description
-            title="Fall Fiesta"
-            subtitle="Off-Season Event - 2017"
-          />
-        </div>
-
-        <div className="flex flex-col items-center ">
-          <Photo
-            className="opacity-50 md:h-[200px] md:w-[500px]"
-            img="/media/IMG_0725.jpg"
-          />
-
-          <ViewAlbum link="https://photos.google.com/share/AF1QipNGkloXktcrqpUPcdl-ett9c8zM8BRmdqL3ohDSTBale0pCd-lUCHfnfKMJi6BCvw?key=d3BzRlpDSGxaWG5BanJ0YWJSZ010TVVKXzZWRERn" />
-
-          <Description
-            title="Ontario Provincial Championships"
-            subtitle="2017"
-          />
-        </div>
-
-        <div className="flex flex-col items-center ">
-          <Photo
-            className="opacity-50 md:h-[200px] md:w-[500px]"
-            img="/media/IMG_4975.jpg"
-          />
-
-          <ViewAlbum link="https://photos.google.com/share/AF1QipOs8_4S1JS0-PUbaGYpGuv9rQjlTG0eeYvNTN-a1SIVvs22ccWQmLRwM5apYisWcQ?key=M2lNVEdhWUcxTWN1LVNDRXMtbmM3U1NUaDdxcGhn" />
-
-          <Description
-            title="McMaster University District Event"
-            subtitle="FIRST Steamworks - 2017"
-          />
-        </div>
-
-        <div className="flex flex-col items-center ">
-          <Photo
-            className="opacity-50 md:h-[200px] md:w-[500px]"
-            img="/media/IMG_4472.jpg"
-          />
-
-          <ViewAlbum link="https://photos.google.com/share/AF1QipNM1C9NNDlah40mJGT0vzwWhOWvmB3cEcLr8j56WygAG_PAesyi-I0uweG26KLWlA?key=Z01aelNVQnNSc1kwQWR4UEQ0M2tDNDFCc0cyNUp3" />
-
-          <Description
-            title="Western University District Event"
-            subtitle="FIRST Steamworks - 2017"
-          />
-        </div>
+                <Description title={media.title} subtitle={media.subtitle} />
+              </div>
+            </>
+          );
+        })}
       </div>
 
       <Footer />
