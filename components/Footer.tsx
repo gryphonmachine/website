0f2f6dbc4eb9c1d19a8ac9c584e1391f0e305383
heybereket <semagnbereket@gmail.com>
2023-04-06 21:05:09 -0400

    footer changes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,9 @@
 export const Footer = () => {
   return (
     <div className="px-4 py-2 pb-16 mt-10 rounded-lg flex flex-col md:flex-row items-center justify-center text-center">
-      <div className="text-gray-400">
-        © {new Date().getFullYear()}. Made with ❤️ by 6070: Gryphon Machine
+      <div className="text-gray-400 text-sm uppercase">
+        Copyright © {new Date().getFullYear()} ⎯{" "}
+        <span className="text-gray-500">Team 6070 Gryphon Machine</span>
       </div>
     </div>
   );
