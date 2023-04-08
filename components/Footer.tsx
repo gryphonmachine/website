723d1ea84bac7dbacc2273edd07a8ce3f0963e6e
heybereket <semagnbereket@gmail.com>
2023-04-08 12:21:38 -0400

    new landing

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export const Footer = () => {
   return (
-    <div className="px-4 py-2 pb-12 mt-10 rounded-lg flex flex-col items-center justify-center text-center">
+    <div className="px-4 py-2 pb-12 mt-10 rounded-lg flex flex-col md:items-center md:justify-center md:text-center pl-10 md:pl-0">
       <div className="text-gray-400 text-sm uppercase mb-3">
         Copyright © {new Date().getFullYear()} ⎯{" "}
         <span className="text-gray-500">Team 6070 Gryphon Machine</span>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,73 @@
 import Head from "next/head";
+import { useState, useEffect } from "react";
 import { Footer } from "@/components/Footer";
 import { Navbar } from "@/components/navbar/Navbar";
+import Image from "next/image";
+import { phrases } from "@/lib/Phrases";
 
 export default function Home() {
+  const [phrase, setPhrase] = useState(phrases[0]);
+  const [usedPhrases, setUsedPhrases] = useState<string[]>([]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      let randomIndex = Math.floor(Math.random() * phrases.length);
+      while (usedPhrases.includes(phrases[randomIndex])) {
+        randomIndex = (randomIndex + 1) % phrases.length;
+      }
+      const newPhrase = phrases[randomIndex];
+      setPhrase(newPhrase);
+      setUsedPhrases([...usedPhrases, newPhrase]);
+      if (usedPhrases.length === phrases.length) {
+        setUsedPhrases([]);
+      }
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, [usedPhrases]);
+
   return (
-    <div className="flex flex-col homeBg bg-[url('/season/6070-steamworks.jpg')] min-h-screen">
+    <>
       <Head>
         <title>6070: Gryphon Machine</title>
       </Head>
-      <div className="flex-grow">
-        <Navbar />
-
-        <div className="flex flex-col md:items-center md:justify-center mt-[150px] md:mt-[250px] md:pl-0 md:pr-0 pl-10 pr-10 flex-grow">
-          <h1 className="font-black text-white text-8xl md:text-9xl">
-            WE ARE <span className="text-primary">6070</span>.
-          </h1>
-          <p className="text-xl font-bold uppercase mt-5 text-gray-400 pl-2">
-            Gryphon Machine
-          </p>
+      <div className="bg-sky-300 bg-opacity-10">
+        <div className="relative h-screen">
+          <Image
+            className="absolute inset-0 w-full h-full object-cover opacity-25"
+            src="/season/6070-steamworks.jpg"
+            alt="Background Image"
+            priority={true}
+            fill
+            style={{ zIndex: -1 }}
+          />
+          <div className="flex flex-col min-h-screen">
+            <Navbar />
+            <div className="flex-grow">
+              <div className="flex flex-col md:items-center md:justify-center mt-[150px] md:mt-[225px] md:pl-0 md:pr-0 pl-10 pr-10 flex-grow">
+                <h1 className="font-black text-white text-8xl md:text-9xl group">
+                  <span className="group-hover:text-primary hover:text-white">
+                    WE ARE
+                  </span>{" "}
+                  <a
+                    href="https://frc-events.firstinspires.org/team/6070"
+                    target="_blank"
+                  >
+                    <span className="text-primary hover:text-white hover:cursor-pointer group-hover:text-white">
+                      6070
+                    </span>
+                    .
+                  </a>
+                </h1>
+                <p className="text-xl font-bold uppercase mt-5 text-gray-400 pl-2">
+                  {phrase}
+                </p>
+              </div>
+            </div>
+            <Footer />
+          </div>
         </div>
       </div>
-
-      <Footer />
-    </div>
+    </>
   );
 }
