c388a7e0237d0b8af512a6444dbbd8caebd96580
heybereket <semagnbereket@gmail.com>
2023-04-07 20:18:43 -0400

    bg image fixes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,14 +1,19 @@
 import Image from "next/image";
 
-export const Footer = () => {
+export const Footer = (props: any) => {
   return (
-    <div className="px-4 py-2 pb-16 mt-10 rounded-lg flex flex-col items-center justify-center text-center">
+    <div
+      className={`${props.className} px-4 py-2 pb-12 mt-10 rounded-lg flex flex-col items-center justify-center text-center`}
+    >
       <div className="text-gray-400 text-sm uppercase mb-3">
         Copyright © {new Date().getFullYear()} ⎯{" "}
         <span className="text-gray-500">Team 6070 Gryphon Machine</span>
       </div>
 
-      <a href="https://www.vercel.com?utm_source=6070-gryphon-machine&utm_campaign=oss" target="_blank">
+      <a
+        href="https://www.vercel.com?utm_source=6070-gryphon-machine&utm_campaign=oss"
+        target="_blank"
+      >
         <Image
           src="/vercel.svg"
           height="175"
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,27 @@
 import Head from "next/head";
 import { Footer } from "@/components/Footer";
 import { Navbar } from "@/components/navbar/Navbar";
-import Image from "next/image";
-import { TeamList } from "@/lib/TeamList";
-import { TeamMember } from "@/components/TeamMember";
-import { Tooltip } from "@nextui-org/react";
-import { FaDiscord, FaTwitter, FaInstagram } from "react-icons/fa";
-import { IoMdMail } from "react-icons/io";
 
 export default function Home() {
   return (
-    <>
+    <div className="flex flex-col homeBg bg-[url('/season/6070-steamworks.jpg')] min-h-screen">
       <Head>
         <title>6070: Gryphon Machine</title>
       </Head>
-      <div className="homeBg bg-[url('/season/6070-steamworks.jpg')] py-1 px-1">
-      <Navbar />
+      <div className="flex-grow">
+        <Navbar />
 
-      <div className=" flex flex-col md:items-center md:justify-center md:mt-[250px] mt-[150px] mb-[350px] pl-10">
-        <h1 className="md:text-9xl text-8xl font-black">
-          WE ARE <span className="text-primary">6070</span>.
-        </h1>
-        <p className="text-gray-400 text-xl font-bold uppercase mt-5">
-          Gryphon Machine
-        </p>
+        <div className="flex flex-col md:items-center md:justify-center mt-[150px] md:mt-[250px] md:pl-0 pl-10 flex-grow">
+          <h1 className="font-black text-8xl md:text-9xl">
+            WE ARE <span className="text-primary">6070</span>.
+          </h1>
+          <p className="text-xl font-bold uppercase mt-5 text-gray-400 pl-2">
+            Gryphon Machine
+          </p>
+        </div>
       </div>
-      
-      <Footer/>
-      </div>
-    </>
+
+      <Footer />
+    </div>
   );
 }
