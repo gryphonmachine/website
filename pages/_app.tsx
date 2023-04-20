e6aaa5c7103d6247f88c8d452b501622fa75603d
heybereket <semagnbereket@gmail.com>
2023-04-19 20:17:44 -0400

    google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,31 @@ import "tailwindcss/tailwind.css";
 
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+          
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-B6X23GK6FM"
+      />
+          
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-B6X23GK6FM', {
+            page_path: window.location.pathname,
+          });
+        `,
+        }}
+      />
       <Head>
         <meta
           name="viewport"
