81ed03af2b89842e737120e16a99692663325a55
heybereket <semagnbereket@gmail.com>
2023-04-20 18:26:09 -0400

    add in first version of ui

--- machine/pages/index.tsx
+++ machine/pages/index.tsx
@@ -1,124 +1,171 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { Footer } from "@/components/Footer";
+import { API_URL } from "@/lib/constants";
+import { GetServerSideProps } from "next";
+import { useState, useEffect } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+export default function Home({ initialTeams }: any) {
+  const [allTeams, setAllTeams] = useState(
+    initialTeams.sort(() => Math.random() - 0.5)
+  );
+  const [query, setQuery] = useState("");
+  const [page, setPage] = useState(0);
 
-export default function Home() {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+
+    const filterTeams = () => {
+      return initialTeams.filter((team: any) =>
+        (
+          team.team_number +
+          team.nickname +
+          team.city +
+          team.state_prov +
+          team.country
+        )
+          .toLowerCase()
+          .includes(query.toLowerCase())
+      );
+    };
+
+    if (query) {
+      setAllTeams(filterTeams());
+    } else {
+      setAllTeams(initialTeams.sort(() => Math.random() - 0.5));
+    }
+  }, [query, initialTeams]);
+
+  const loadMore = async () => {
+    const nextPage = page + 1;
+    const response = await fetch(`${API_URL}/api/teams?page=${nextPage}`);
+    const newTeams = await response.json();
+    setAllTeams([...allTeams, ...newTeams]);
+    setPage(nextPage);
+  };
+
+  const changeSearch = (event: { target: { value: string } }) => {
+    setQuery(event.target.value);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <>
+      {isClient && (
+        <>
+          <div className="flex flex-col justify-center items-center">
+            <h1 className="text-center text-primary md:text-5xl text-3xl font-black mt-16 mb-2">
+              Machine: Scouting Simplified
+            </h1>
+            <p className="text-gray-400">
+              6070 presents Machine - I need to rewrite this.
+            </p>
+            <input
+              type="text"
+              placeholder="Search teams..."
+              value={query}
+              onChange={changeSearch}
+              spellCheck="false"
+              className="rounded-lg bg-gray-700 py-2 px-5 mt-5 md:pr-4 md:pl-4 pr-8 pl-8 md:w-[450px]"
             />
-          </a>
-        </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+            <div className="flex flex-col md:grid md:grid-cols-5 gap-5 mt-10 md:pr-32 md:pl-32 pr-8 pl-8">
+              {allTeams.map((team: any, key: number) => {
+                return (
+                  <a
+                    key={key}
+                    href={`https://frc-events.firstinspires.org/team/${team.team_number}`}
+                    target="_blank"
+                  >
+                    <div className="px-5 py-10 bg-gray-700 border-2 border-gray-500 rounded-lg hover:bg-gray-600">
+                      <h1 className="text-gray-200 font-black">
+                        {team.nickname.length > 19
+                          ? `${team.nickname.slice(0, 19)}...`
+                          : team.nickname}
+                      </h1>
+                      <p className="text-gray-400 text-xs uppercase">
+                        {team.city ? team.city : "No location"}
+                      </p>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+                      <p className="text-gray-400 font-bold text-lg">
+                        FRC {team.team_number}
+                      </p>
+                    </div>
+                  </a>
+                );
+              })}
+            </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+            <button
+              onClick={loadMore}
+              className="rounded-lg bg-gray-700 py-2 px-5 mt-5 text-gray-200 hover:bg-gray-600"
+            >
+              Load more
+            </button>
+          </div>
+          <Footer />
+        </>
+      )}
+    </>
+  );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({
+  req,
+  res,
+  query,
+}) => {
+  res.setHeader(
+    "Cache-Control",
+    "public, s-maxage=10, stale-while-revalidate=59"
+  );
+  const baseFetch = async (pageNum: string) =>
+    await fetch(`${API_URL}/api/teams?page=${pageNum}`).then((res) =>
+      res.json()
+    );
+
+  const page1 = await baseFetch("1");
+  const page2 = await baseFetch("2");
+  const page3 = await baseFetch("3");
+  const page4 = await baseFetch("4");
+  const page5 = await baseFetch("5");
+  const page6 = await baseFetch("6");
+  const page7 = await baseFetch("7");
+  const page8 = await baseFetch("8");
+  const page9 = await baseFetch("9");
+  const page10 = await baseFetch("10");
+  const page11 = await baseFetch("11");
+  const page12 = await baseFetch("12");
+  const page13 = await baseFetch("13");
+  const page14 = await baseFetch("14");
+  const page15 = await baseFetch("15");
+  const page16 = await baseFetch("16");
+  const page17 = await baseFetch("17");
+  const page18 = await baseFetch("18");
+  const page19 = await baseFetch("19");
+
+  return {
+    props: {
+      initialTeams: [
+        ...page1,
+        ...page2,
+        ...page3,
+        ...page4,
+        ...page5,
+        ...page6,
+        ...page7,
+        ...page8,
+        ...page9,
+        ...page10,
+        ...page11,
+        ...page12,
+        ...page13,
+        ...page14,
+        ...page15,
+        ...page16,
+        ...page17,
+        ...page18,
+        ...page19,
+      ],
+    },
+  };
+};
