5bac4921723b9ee71f62ed3f956e832245bbb3ee
heybereket <semagnbereket@gmail.com>
2023-04-23 11:36:08 -0400

    move files back

--- machine/pages/api/events/all.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { fetchData } from "@/lib/fetchData";
-
-export default async function getTeams(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  return await fetchData(res, `events/2023`);
-}
