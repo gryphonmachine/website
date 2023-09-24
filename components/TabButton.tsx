b551a00037690960ec60465cb4243198740a871e
Advay Chandorkar <advay1612@icloud.com>
2023-09-24 16:44:08 -0400

    yellow indicator on tab button

--- components/TabButton.tsx
+++ components/TabButton.tsx
@@ -11,9 +11,9 @@ export const TabButton = (props: Props) => {
   return (
     <button
       onClick={props.onClick}
-      className={`px-5 py-2 rounded-lg font-semibold transition-all duration-300 ${
+      className={`px-5 py-2 rounded-lg font-semibold  ${
         props.active === props.tab
-          ? "bg-[#191919] text-white"
+          ? "bg-[#191919] text-[#ffbb08]"
           : "text-gray-400 bg-[#191919] dark:hover:border-gray-600 border-2 border-[#2A2A2A] hover:text-white"
       }`}
     >
