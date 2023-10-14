b83dbd98886eec8c92b43fd5602562ba2dc87c2c
Bereket Semagn <semagnbereket@gmail.com>
2023-10-13 21:41:11 -0400

    fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  experimental: {
+    largePageDataBytes: 20000 * 100000,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
