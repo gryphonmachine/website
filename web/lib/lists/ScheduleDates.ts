bed958b058ecb441a8bef6bc2e116ea5481fb166
advay-c <advay1612@icloud.com>
2023-12-26 21:49:01 -0500

    add first kickoff and start of build season for schedule

--- web/lib/lists/ScheduleDates.ts
+++ web/lib/lists/ScheduleDates.ts
@@ -1,12 +1,6 @@
 import { ScheduleEvent } from "@/pages/schedule";
 
 export const scheduleDates: ScheduleEvent[] = [
-  { date: "2023-10-10", event: "Meeting" },
-  { date: "2023-10-11", event: "Meeting" },
-  { date: "2023-10-17", event: "Meeting" },
-  { date: "2023-10-18", event: "Meeting" },
-  { date: "2023-10-24", event: "Meeting" },
-  { date: "2023-10-25", event: "Meeting" },
-  { date: "2023-10-25", event: "Smash qualif" },
-  { date: "2023-10-26", event: "Smash Bracket" },
+  { date: "2024-01-06", event: "First Crescendo Kickoff" },
+  { date: "2024-01-07", event: "Start Of Build Season" },
 ];
